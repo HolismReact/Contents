6622bc3c2a8eb7e9e905cb4328a3fdf9f5d1387b
Saeed Nemati <sn.1361@gmail.com>
2022-02-05 17:05:53 +0330

    committed by script

--- Admin/Slide/List.js
+++ Admin/Slide/List.js
@@ -1,1 +1,22 @@
+import { List, Text } from '@List'
 
+const filters = <>
+    <Text
+        column=''
+    />
+</>
+
+const Slides = () => {
+    return <List
+        title='Slides'
+        entityType='Slide'
+        filters={filters}
+        sorts={sorts}
+        headers={headers}
+        row={row}
+        create={CreateSlide}
+        hasDelete={true}
+    />
+}
+
+export default Slides
