9961c0ba992b1a182675442fae3cb1024492a1d6
Saeed Nemati <sn.1361@gmail.com>
2022-02-16 15:33:11 +0330

    committed by script

--- Admin/Text/List.js
+++ Admin/Text/List.js
@@ -1,7 +1,26 @@
-import { Form } from '@Form'
+import { List } from '@List'
+import UpdateText from './Update'
+
+const headers = <>
+    <th>Title</th>
+    <th>Value</th>
+</>
+
+const row = (item) => {
+    return <>
+        <td>{item.title}</td>
+        <td>{item.value}</td>
+    </>
+}
 
 const Texts = () => {
-    return <div>texts</div>
+    return <List
+        title='Texts'
+        entityType='StaticText'
+        headers={headers}
+        row={row}
+        edit={UpdateText}
+    />
 }
 
 export default Texts
